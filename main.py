676cc27386dac9f4ae79fbf27d9c05d6abea2bf7
MrDave <davidschnoll@gmail.com>
2023-06-12 15:05:56 +0300

    Created function to download Nasa's APOD

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 from urllib import parse
 from pathlib import Path
+from dotenv import load_dotenv
 
 
 def download_picture(url, path):
@@ -31,10 +32,36 @@ def get_file_extension(url):
     return file_extension
 
 
+def fetch_nasa_pictures(token="DEMO_KEY", count=None):
+    url = "https://api.nasa.gov/planetary/apod"
+    params = {
+        "api_key": token,
+        "count": count
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+
+    pictures = []
+    for picture in response.json():
+        try:
+            pictures.append(picture["url"])
+
+        except KeyError:
+            continue
+    
+    for number, picture in enumerate(pictures):
+        extension = get_file_extension(picture)
+        path = f"images/nasa_apod_{number}{extension}"
+        download_picture(picture, path)
+
+
 def main():
     Path("images/").mkdir(exist_ok=True)
-
-    fetch_spacex_pictures("5eb87d42ffd86e000604b384")
+    load_dotenv()
+    nasa_token = os.getenv("NASA_TOKEN")
+    # fetch_spacex_pictures("5eb87d42ffd86e000604b384")
+    fetch_nasa_pictures(nasa_token, 40)
 
 
 if __name__ == "__main__":

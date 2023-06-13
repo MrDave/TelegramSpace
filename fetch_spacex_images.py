020ab3e24f4cbe4cc7d64f8bf8a3ddd605ad2f41
MrDave <davidschnoll@gmail.com>
2023-06-13 23:37:51 +0300

    Separated SpaceX script

---
+++ fetch_spacex_images.py
@@ -0,0 +1,18 @@
+import argparse
+import requests
+from file_functions import download_image
+
+
+parser = argparse.ArgumentParser(description="Download images from SpaceX's launches")
+parser.add_argument("--id", help="ID of SpaceX launch. Latest by default", default="latest")
+args = parser.parse_args()
+launch_id = str(args.id)
+
+url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
+response = requests.get(url)
+response.raise_for_status()
+
+images = response.json()["links"]["flickr"]["original"]
+
+for number, image in enumerate(images):
+    download_image(image, f"images/spacex_{number}.jpg")

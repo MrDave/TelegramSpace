f0decd396209ec2dbe84cad0afaffda6dbb62948
MrDave <davidschnoll@gmail.com>
2023-06-13 23:37:27 +0300

    Separated file handling functions

---
+++ file_functions.py
@@ -0,0 +1,21 @@
+import requests
+import os
+from urllib import parse
+from pathlib import Path
+
+
+def download_image(url, path):
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    Path("images/").mkdir(exist_ok=True)
+
+    with open(path, "wb") as file:
+        file.write(response.content)
+
+
+def get_file_extension(url):
+    url_path = parse.urlsplit(url)
+    file_extension = os.path.splitext(url_path.path)[1]
+    return file_extension

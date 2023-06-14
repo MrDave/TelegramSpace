4cf7f03aacdae29316ce3e10144cb4353bed66ce
MrDave <davidschnoll@gmail.com>
2023-06-14 13:04:42 +0300

    Publish images to telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,6 +5,7 @@ from file_functions import load_token
 token = load_token("TG_BOT_TOKEN")
 bot = telegram.Bot(token)
 
-chat_id = load_token("TG_TEST_CHAT")
-text = "There are some, who call me... Tim"
-bot.send_message(chat_id=chat_id, text=text)
+chat_id = "@DaveSpacePhotos"
+
+with open("images/nasa_apod_0.jpg", "rb") as file:
+    bot.send_document(chat_id=chat_id, document=file)

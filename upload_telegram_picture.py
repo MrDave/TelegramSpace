589173210092a0c2c10ddacd968465fa46715b0e
MrDave <davidschnoll@gmail.com>
2023-06-16 19:04:14 +0300

    Support random and specific files

--- upload_telegram_picture.py
+++ upload_telegram_picture.py
@@ -2,7 +2,7 @@ import telegram
 from file_functions import load_token
 from file_functions import get_image_paths
 import argparse
-from random import shuffle
+from random import choice
 from time import sleep
 
 
@@ -18,15 +18,19 @@ def upload_image(image):
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Upload a random photo to Telegram channel")
     parser.add_argument(
-        "file",
-        help="path of file to upload. Example: \"images\\image.jpg\"",
+        "-f",
+        "--file",
+        help="path of specific file to upload. Example: \"images\\image.jpg\"",
         type=str
     )
     args = parser.parse_args()
     image = args.file
 
+    if image is None:
+        images = get_image_paths()
+        image = choice(images)
     upload_image(image)
 
 
